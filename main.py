abe5d983ad9bb9ef64b3d8cbbe5d6fd83eee89ed
Muhammad Sunaam <msunaam2002@gmail.com>
2024-04-02 01:29:25 +0500

    file upload

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# 16 megabytes size limit
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/upload-image", methods=["POST"])
+def upload_image():
+    if request.method != 'POST':
+        return "Method not allowed. Please send a POST request."
+    
+    if 'file' not in request.files:
+        return "No file part"
+    
+    file = request.files['file']
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return f"File uploaded successfully {filename}"
